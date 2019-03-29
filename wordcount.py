129235ee6ac00786399e615000c005b7bd94812d
lizj <lizj@mapbar.com>
2019-03-29 11:11:41 +0800

    Fix directory

--- wordcount.py
+++ wordcount.py
@@ -2,9 +2,9 @@ import os
 
 result = dict()
 
-for _, _, filenames in os.walk('./testdata'):
+for dir, _, filenames in os.walk('./testdata'):
     for filename in filenames:
-        with open(filename) as f:
+        with open(dir + './' + filename) as f:
             for word in f.read().split():
                 if word in result:
                     result[word] += 1
