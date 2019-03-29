b8ba62330a554e7938e7c73891a70710468a4dc4
lizj <lizj@mapbar.com>
2019-03-29 17:10:28 +0800

    It's safe to assure zero-inited see https://stackoverflow.com/questions/2667355/mapint-int-default-values

--- wordcount.boost.cpp
+++ wordcount.boost.cpp
@@ -4,19 +4,13 @@
 #include <fstream>
 #include <boost/filesystem.hpp>
 
-int main()
-{
+int main() {
 	std::unordered_map<std::string, int> records;
-	for (auto& dirIter : boost::filesystem::recursive_directory_iterator("./testdata"))
-	{
-		if (boost::filesystem::is_regular_file(dirIter.status()))
-		{
+	for (auto& dirIter : boost::filesystem::recursive_directory_iterator("./testdata")) {
+		if (boost::filesystem::is_regular_file(dirIter.status())) {
 			std::ifstream f(dirIter.path().string());
 			for (std::istream_iterator<std::string> wordIter(f), end; wordIter != end; wordIter++)
-				if (records.find(*wordIter) != records.end())
-					records[*wordIter]++;
-				else
-					records[*wordIter] = 1;
+				records[*wordIter]++;
 		}
 	}
 	for (auto& iter : records)
