95c939fb504112ab6c847710cebc1021604dc3d2
Zhujin Li <lizhujin@outlook.com>
2019-03-30 01:23:02 +0800

    Revert some changes

--- wordcount.boost.cpp
+++ wordcount.boost.cpp
@@ -2,12 +2,12 @@
 #include <string>
 #include <unordered_map>
 #include <fstream>
-#include <experimental/filesystem>
+#include <boost/filesystem.hpp>
 
 int main() {
 	std::unordered_map<std::string, int> records;
-	for (auto& dirIter : std::filesystem::recursive_directory_iterator("./testdata")) {
-		if (std::filesystem::is_regular_file(dirIter.status())) {
+	for (auto& dirIter : boost::filesystem::recursive_directory_iterator("./testdata")) {
+		if (boost::filesystem::is_regular_file(dirIter.status())) {
 			std::ifstream f(dirIter.path().string());
 			for (std::istream_iterator<std::string> wordIter(f), end; wordIter != end; wordIter++)
 				records[*wordIter]++;
