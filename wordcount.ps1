f8f3007be6b8fc04f8904aefd5ed4b46ba619aa7
Zhujin (Connor) Li <lizhujin@outlook.com>
2021-10-01 23:13:35 +0800

    Add PowerShell version

---
+++ wordcount.ps1
@@ -0,0 +1,12 @@
+﻿$records = @{}
+$files = Get-ChildItem -Path .\testdata -Recurse -File
+foreach ($file in $files)
+{
+    $content = (Get-Content $file.FullName -Raw).ToString()
+    $words = $content -split '\s+'
+    foreach ($word in $words)
+    {
+        $records[$word]++
+    }
+}
+Write-Output $records
